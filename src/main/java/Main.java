7a0994798874090cc6c24c69c0e64ea253c666fe
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:07:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+    
+   // Fazer um programa que le um valor inteiro N e depois N numeros inteiros. ao final mostrar a soma dos N números lidos
+
+    for( int N = sc.nextInt)
+
+
+
+
+
+
+    
+
+
+   //for(int i = 0; i <5; i++ ){
+   //  System.out.println(i);
+   }
+    //for(int j = 4; j >= 0; j--) {
+     // System.out.println(j);
+    
+
+    //for( inicio ; condição; incrimento){ 
+  //comando
+
+
+  
+  //}
+
+
+
+
+  
+  
+    sc.close();
+
+
+  
+    
